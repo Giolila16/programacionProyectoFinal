c5f68a2798fddd5f27c290b8a2657e90d8ac2793
kdeke <kdeke@10.10.12.157>
2025-05-24 22:07:56 -0500

    no puedo marta

--- src/Formularios/iniciosesion.java
+++ src/Formularios/iniciosesion.java
@@ -11,14 +11,13 @@ import javax.swing.JPanel;
 
 /**
  *
- * @author USUARIO
+ * @author kdeke
  */
-public class Inicio extends javax.swing.JFrame {
+public class iniciosesion extends javax.swing.JFrame {
 
-    FondoPanel fondo = new FondoPanel();
+    iniciosesion.FondoPanel fondo = new iniciosesion.FondoPanel();
     
-    public Inicio() {
-        
+    public iniciosesion() {
         this.setContentPane(fondo);
         initComponents();
         this.setLocationRelativeTo(this);
@@ -33,44 +32,26 @@ public class Inicio extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new FondoPanel();
-        Seguir = new javax.swing.JButton();
+        jPanel1 = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
-
-        Seguir.setForeground(new java.awt.Color(255, 255, 255));
-        Seguir.setText("CONSEGUIR UNA CASA");
-        Seguir.setOpaque(true);
-        Seguir.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                SeguirActionPerformed(evt);
-            }
-        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(287, 287, 287)
-                .addComponent(Seguir)
-                .addContainerGap(548, Short.MAX_VALUE))
+            .addGap(0, 754, Short.MAX_VALUE)
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(405, Short.MAX_VALUE)
-                .addComponent(Seguir, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(139, 139, 139))
+            .addGap(0, 491, Short.MAX_VALUE)
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -79,17 +60,6 @@ public class Inicio extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void SeguirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SeguirActionPerformed
-        Seguir.setContentAreaFilled(false);
-        Seguir.setFocusPainted(false);        // Quita el borde de enfoque al hacer clic
-Seguir.setBorderPainted(false);       // No pinta bordes
-Seguir.setOpaque(false);
-        iniciosesion inicio = new iniciosesion();
-        
-        inicio.setVisible(true);
-        this.setVisible(false);
-    }//GEN-LAST:event_SeguirActionPerformed
 
     /**
      * @param args the command line arguments
@@ -108,30 +78,29 @@ Seguir.setOpaque(false);
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Inicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(iniciosesion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Inicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(iniciosesion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Inicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(iniciosesion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Inicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(iniciosesion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Inicio().setVisible(true);
+                new iniciosesion().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton Seguir;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 
-    class FondoPanel extends JPanel
+class FondoPanel extends JPanel
     {
         private Image imagon;
         
@@ -145,6 +114,4 @@ Seguir.setOpaque(false);
             super.paint(g);
         }
     }
-    
-         
 }
